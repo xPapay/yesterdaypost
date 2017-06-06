2ba9e0e2621d580cb33bbc5a13cd437480d7c21f
xPapay <papay.lukas@gmail.com>
2017-06-06 13:52:12 +0200

    Replace logo by image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
 		</div>
 	</div>
 	<div class="col-md-8 col-sm-12 d-flex justify-content-center align-items-end">
-		<h1 class="text-uppercase text-center brand">Yesterday Post</h1>
+    <div class="logo"></div>
 	</div>
 	<div class="col-sm-2 hidden-sm-down d-flex align-items-end text-center">
 		<div class="advertisement">
