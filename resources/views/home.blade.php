16606724c930e5224fb8f933aa13cce3179e7ccc
xPapay <papay.lukas@gmail.com>
2017-06-11 19:14:34 +0200

    Improve vertical spacing between articles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -78,7 +78,7 @@
 		</div>
 	</div> <!-- .left-section -->
 	<div class="col-md-3 col-sm-5 d-flex flex-column justify-content-between border-right-md third-column">
-		@foreach ($columns[2] as $article)
+		@foreach ($columns[1] as $article)
 			<div class="article @if($loop->last) no-separator-md @endif full-width-separator">
 				<div class="article__headline separator">
 					<a href="{{ $article->url }}" target="_blank"><h2>@trim_dot($article->headline)</h2></a>
@@ -91,7 +91,7 @@
 		@endforeach
 	</div>
 	<div class="col-md-3 d-flex flex-column justify-content-between fourth-column">
-		@foreach ($columns[1] as $article)
+		@foreach ($columns[2] as $article)
 			<div class="article @if($loop->last) no-separator-md @endif full-width-separator">
 				<div class="article__headline separator">
 					<a href="{{ $article->url }}" target="_blank"><h2>@trim_dot($article->headline)</h2></a>
