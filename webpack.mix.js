09f18e562139cc5a8d3b111943e856678f1f6463
xPapay <papay.lukas@gmail.com>
2017-06-13 12:35:35 +0200

    Add assets versioning for cache busting

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,7 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
    .copyDirectory('resources/assets/images', 'public/images');
+
+if (mix.config.inProduction) {
+    mix.version();
+}
